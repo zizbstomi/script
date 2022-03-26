2a35924960e253b3a14eee607ddf04a96b6a432c
M. Abdul Aziz Albustomi <100144229+zizbstomi@users.noreply.github.com>
2022-03-26 13:00:48 +0700

    Add files via upload

---
+++ zizbstomiscript.js
@@ -0,0 +1,9 @@
+async function zizbstomi(){
+await swals.fire({
+  title: '<strong>Mau bikin kya gini juga??</strong>',
+  html:
+    'DM aja di ' +
+    '<a href="https://linktr.ee/zizbstomi">https://linktr.ee/zizbstomi</a> ' +
+    'ya!' + '<br><br>' + 'Jangan lupa follow social media admin ya, hhe:V',
+  focusConfirm: false,
+});} 
